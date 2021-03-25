240d0ca02bfb7afb80a41b7ee0f39c6f449eb8f6
Brayan Forero <brayanroswll2802@gmail.com>
2021-03-24 20:31:41 -0400

    setup basic server

--- src/app.js
+++ src/app.js
@@ -1,6 +1,17 @@
-const express = require('express')
-const app = express()
+const express = require('express');
+const app = express();
+const morgan = require('morgan');
+// SETTINGS
+app.set('PORT', process.env.PORT || 3000);
+app.set('APPNAME', process.env.APP_NAME || 'API Virtual Store');
 
+// MIDDLEWARES
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+app.use(morgan('dev'));
 
-
-module.exports = app
+// PUBLIC STATICS
+// GLOBAL VARIABLES
+// ROUTES
+// EXPORTIN APP SERVER
+module.exports = app;
