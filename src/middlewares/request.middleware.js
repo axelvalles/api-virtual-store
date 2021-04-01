e2c392d4b178a321320dcd136d762ca05ef3db91
Brayan Forero <brayanroswll2802@gmail.com>
2021-04-01 03:36:26 -0400

    fix middleware reques json

--- src/middlewares/request.middleware.js
+++ src/middlewares/request.middleware.js
@@ -1,11 +1,20 @@
 const requestJson = (req, res, next) => {
-	if (req.headers['content-type'] !== 'application/json' && req.is('json'))
+	if (req.method !== 'GET') {
+		if (req.headers['content-type'] !== 'application/json' && !req.is('json'))
+			return res.status(401).json({
+				status: 401,
+				error: 'UNAUTHORIZED',
+				message: 'Request Invalid',
+			});
+		next();
+	}
+
+	if (req.headers['content-type'] !== 'application/json')
 		return res.status(401).json({
 			status: 401,
 			error: 'UNAUTHORIZED',
 			message: 'Request Invalid',
 		});
-
 	next();
 };
 
