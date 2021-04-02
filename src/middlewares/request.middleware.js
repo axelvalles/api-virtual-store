d5ede18ecbb367cd6957b253c2bc1cd1f6ea0ca9
Brayan Forero <brayanroswll2802@gmail.com>
2021-04-02 08:45:59 -0400

    refactor middleware request is json

--- src/middlewares/request.middleware.js
+++ src/middlewares/request.middleware.js
@@ -1,5 +1,14 @@
 const requestJson = (req, res, next) => {
-	if (req.method !== 'GET') {
+	if (req.method === 'GET') {
+		if (req.headers['content-type'] !== 'application/json')
+			return res.status(401).json({
+				status: 401,
+				error: 'UNAUTHORIZED',
+				message: 'Request Invalid',
+			});
+
+		next();
+	} else {
 		if (req.headers['content-type'] !== 'application/json' && !req.is('json'))
 			return res.status(401).json({
 				status: 401,
@@ -8,14 +17,6 @@ const requestJson = (req, res, next) => {
 			});
 		next();
 	}
-
-	if (req.headers['content-type'] !== 'application/json')
-		return res.status(401).json({
-			status: 401,
-			error: 'UNAUTHORIZED',
-			message: 'Request Invalid',
-		});
-	next();
 };
 
 module.exports = requestJson;
